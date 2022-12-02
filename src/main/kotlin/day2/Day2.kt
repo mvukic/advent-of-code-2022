840ce9399d2e50572d37c27a3131512169335cb4
mvukic <matija.vukic2@gmail.com>
2022-12-02 19:05:20 +0100

    updates

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -3,13 +3,43 @@ package day2
 import getFileLines
 
 fun part1() {
-    val calories = getFileLines("day2.txt")
-
+    val games = getFileLines("day2.txt")
+        .mapNotNull {
+            when(it) {
+                "A X" -> 3 + 1
+                "A Y" -> 6 + 2
+                "A Z" -> 0 + 3
+                "B X" -> 0 + 1
+                "B Y" -> 3 + 2
+                "B Z" -> 6 + 3
+                "C X" -> 6 + 1
+                "C Y" -> 0 + 2
+                "C Z" -> 3 + 3
+                else -> null
+            }
+        }
+        .sum()
+    println(games)
 }
 
 fun part2() {
-    val calories = getFileLines("day2.txt")
-
+    val games = getFileLines("day2.txt")
+        .mapNotNull {
+            when(it) {
+                "A X" -> 0 + 3
+                "A Y" -> 3 + 1
+                "A Z" -> 6 + 2
+                "B X" -> 0 + 1
+                "B Y" -> 3 + 2
+                "B Z" -> 6 + 3
+                "C X" -> 0 + 2
+                "C Y" -> 3 + 3
+                "C Z" -> 6 + 1
+                else -> null
+            }
+        }
+        .sum()
+    println(games)
 }
 
 fun main() {
